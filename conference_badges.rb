aa9423a7fe0784494355327c4f8e9fbce4739659
Elizabeth Zevin <ehzevin@gmail.com>
2019-02-11 18:19:12 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,12 +6,12 @@ end
 
 def batch_badge_creator(attendees)
   attendees.map do |name|  
-  badge_maker(name) 
-end
+    badge_maker(name) 
+  end
 end
 
 def assign_rooms(attendees)
-  attendees.each_with_index do |name, index|
+  attendees.map do |name, index|
    return "Hello, #{name}! You'll be assigned to room #{index+1}!"
 end 
 end
