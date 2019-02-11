ca10dc9243d0adbda072684803b406f85b096ecd
Elizabeth Zevin <ehzevin@gmail.com>
2019-02-11 18:12:23 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,9 +5,8 @@ def badge_maker(name)
 end 
 
 def batch_badge_creator(attendees)
-  attendees.each do |name|
-  badge_maker.push
-  
+  attendees.each do |name|  
+    return badge_maker(name) 
 end
 end
 
